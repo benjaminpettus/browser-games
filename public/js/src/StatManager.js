f79417256f19f2c0acc717c2a19b20eb569b8230
benjaminpettus <benjaminpettus@gmail.com>
2017-05-10 10:45:04 -0700

    breaking code into modules

---
+++ public/js/src/StatManager.js
@@ -0,0 +1,27 @@
+
+
+define(function() {
+
+  var StatManager = Class.extend({
+
+    init: function() {
+      this.reset(0);
+    },
+
+    reset: function() {
+
+      this.tetraminos = {
+        L:0,
+        I:0,
+        T:0,
+        S:0,
+        Z:0,
+        O:0,
+        J:0,
+
+        tot: 0,
+      }
+    }
+  });
+  return StatManager
+});
