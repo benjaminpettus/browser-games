15be85394d06c9d761a2535e5852f5e4691ddcc2
benjaminpettus <benjaminpettus@gmail.com>
2017-05-11 13:55:26 -0700

    added score and styling

--- public/tetris.js
+++ public/tetris.js
@@ -4,15 +4,19 @@ const context = canvas.getContext('2d');
 context.scale( 20, 20 );
 
 function arenaSweep() {
-  outer: for (let y = arena.length - 1; y < 0; --y ){
+  let rowCount = 1;
+  outer: for (let y = arena.length - 1; y > 0; --y ){
     for (let x = 0; x < arena[y].length; ++x) {
-      if (arena[y][x] ===0) {
+      if (arena[y][x] === 0) {
         continue outer;
       }
     }
     const row = arena.splice(y, 1)[0].fill(0);
     arena.unshift(row);
     ++y;
+
+    player.score += rowCount * 10;
+    rowCount *= 2;
   }
 }
 
@@ -123,6 +127,7 @@ function playerDrop() {
     merge( arena, player );
     playerReset();
     arenaSweep();
+    updateScore();
   }
     dropCounter = 0;
 }
@@ -142,6 +147,8 @@ function playerReset() {
                  (player.matrix[0].length / 2 | 0);
   if (collide(arena, player)) {
     arena.forEach(row => row.fill(0));
+    player.score = 0;
+    updateScore();
   }
 }
 
@@ -231,4 +238,5 @@ document.addEventListener('keydown', event => {
 });
 
 playerReset();
+updateScore();
 update();
